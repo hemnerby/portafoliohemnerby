210347ecdb8c937861279711866a5f093ba7fa42
hemnerby <delarosatejada07@gmail.com>
2024-01-19 17:55:37 -0400

    portafoliohemnerby

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// toggleicon navbar
+
+
+//scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+    // sticky header
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
